db565a371f06be2cc71f5385aa8cde30e5e0a052
ayoubDevT <ayoubyafouze@gmail.com>
2022-09-21 15:56:58 +0100

    final push berfore translation

--- resources/views/components/clientLayout.blade.php
+++ resources/views/components/clientLayout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atlas Outdoor</title>
     <link rel="stylesheet" href="{{ asset('assets/clientsite/style/style.css') }}">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
