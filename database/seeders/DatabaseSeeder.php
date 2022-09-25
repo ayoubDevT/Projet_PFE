345778a6d15daca2b2ed91666c4bb2ac64948c23
ayoubDevT <ayoubyafouze@gmail.com>
2022-09-25 23:32:18 +0100

    push

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Page;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,46 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Page::create([
+            'name_en' => 'home',
+            'slug' => 'home',
+            
+        ]);
+        Page::create([
+            'name_en' => 'faqs',
+            'slug' => 'faqs',
+            
+        ]);
+        Page::create([
+            'name_en' => 'videos',
+            'slug' => 'videos',
+            
+        ]);
+        Page::create([
+            'name_en' => 'contact',
+            'slug' => 'contact',
+            
+        ]);
+        Page::create([
+            'name_en' => 'about',
+            'slug' => 'about',
+            
+        ]);
+        Page::create([
+            'name_en' => 'our social projects',
+            'slug' => 'our-social-projects',
+            
+        ]);
+        Page::create([
+            'name_en' => 'Prepare your travel to morocco',
+            'slug' => 'Prepare-your-travel-to-morocco',
+            
+        ]);
+        Page::create([
+            'name_en' => 'General advices',
+            'slug' => 'General-advices',
+            
+        ]);
     }
 }
