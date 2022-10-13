0ace65057e1887797837802f41f63eabcd5292ff
ayoubDevT <ayoubyafouze@gmail.com>
2022-10-13 03:47:34 +0100

    push

--- resources/views/admin/messages/message.blade.php
+++ resources/views/admin/messages/message.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-12 grid-margin stretch-card">
                 <div class="card">
                     <div class="card-body">
-                        <h6 class="card-title">Bookings</h6>
+                        <h6 class="card-title">Messages</h6>
 
                         <div class="table-responsive">
                             <table id="dataTableExample" class="table">
